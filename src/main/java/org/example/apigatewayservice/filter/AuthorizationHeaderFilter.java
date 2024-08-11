8e3ed512e096bfb9d0a536ca1738c88a1dd8a373
pyeondohun <dohoon321@gmail.com>
2024-08-11 20:33:48 +0900

    암호화 진행중

--- src/main/java/org/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
+++ src/main/java/org/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -55,6 +55,7 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
 
         String subject=null;
         try {
+            log.info("token.secret : {}", env.getProperty("token.secret"));
             subject = Jwts.parser()
                     .setSigningKey(env.getProperty("token.secret").getBytes(StandardCharsets.UTF_8))
                     .parseClaimsJws(jwt)
